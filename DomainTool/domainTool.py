87ef254f7470cb175e2092665da25a78eab2bd54
Ap6pack <adamslinuxemail@gmail.com>
2023-05-25 12:52:09 -0400

    reporg'd code, added  more error handling/feedback

--- DomainTool/domainTool.py
+++ DomainTool/domainTool.py
@@ -1,14 +1,17 @@
-import os
-import xml.etree.ElementTree as ET
-import openpyxl
-import whois
-import dns.resolver
-import ssl
-import requests
-import OpenSSL.crypto
+import sys
 import subprocess
+import tkinter as tk
 from tkinter import Tk
+from tkinter import filedialog
 from tkinter.filedialog import askopenfilename, asksaveasfilename
+
+import dns.resolver
+import openpyxl
+import OpenSSL.crypto
+import requests
+import ssl
+import whois
+import xml.etree.ElementTree as ET
 
 
 class DataProcessor:
@@ -17,19 +20,22 @@ class DataProcessor:
         self.results = []
 
     def load_data_from_excel(self, file_path):
+        if not file_path:
+            print("No file selected. Exiting...")
+            return False
         try:
-            # Code to read data from Excel file
             wb = openpyxl.load_workbook(file_path)
-            # Assume the data is in the first sheet and the first column
             sheet = wb.active
-            self.data = [cell.value for cell in sheet["A"]]
-            wb.close()
+            self.data = []
+            for row in sheet.iter_rows(values_only=True):
+                self.data.append(row[0])
+            print(f"Data loaded from Excel file: {file_path}")
+            return True
         except Exception as e:
-            raise ValueError("Failed to load data from Excel file. Error: " + str(e))
-
+            raise ValueError(f"Failed to load data from Excel file. Error: {str(e)}")
+   
     def load_data_from_xml(self, file_path):
         try:
-            # Code to read data from XML file
             tree = ET.parse(file_path)
             root = tree.getroot()
             self.data = [elem.text for elem in root.findall(".//data")]
@@ -38,14 +44,12 @@ class DataProcessor:
 
     def load_data_from_text(self, file_path):
         try:
-            # Code to read data from text file
             with open(file_path, "r") as file:
                 self.data = file.read().splitlines()
         except Exception as e:
             raise ValueError("Failed to load data from Text file. Error: " + str(e))
 
     def load_data_from_terminal(self):
-        # Code to read data from user input in the terminal
         print("Enter data (one entry per line, press 'enter' twice to continue):")
         self.data = []
         entry = input()
@@ -55,241 +59,224 @@ class DataProcessor:
 
     def perform_functions(self, function_options):
         for option in function_options:
-            if option.strip() == "a":
-                # Perform whois lookup
-                for domain in self.data:
-                    result = self.perform_whois_lookup(domain)
-                    self.results.append(result)
-            elif option.strip() == "b":
-                # Perform nslookup
-                for domain in self.data:
-                    result = self.perform_nslookup(domain)
-                    self.results.append(result)
-            elif option.strip() == "c":
-                # Perform DNS lookup
-                for domain in self.data:
-                    result = self.perform_dns_lookup(domain)
-                    self.results.append(result)
-            elif option.strip() == "d":
-                # Perform reverse DNS lookup
-                for ip in self.data:
-                    result = self.perform_reverse_dns_lookup(ip)
-                    self.results.append(result)
-            elif option.strip() == "e":
-                # Perform SSL cert scan
-                for domain in self.data:
-                    result = self.perform_ssl_cert_lookup(domain)
-                    self.results.append(result)
-            elif option.strip() == "f":
-                # Perform HTTP headers lookup
-                for url in self.data:
-                    result = self.perform_http_headers_lookup(url)
-                    self.results.append(result)
-            elif option.strip() == "g":
-                # Perform subdomain lookup
-                for domain in self.data:
-                    result = self.perform_subdomain_lookup(domain)
-                    self.results.append(result)
+            option = option.strip()
+            if option == "a":
+                self.perform_whois_lookup()
+            elif option == "b":
+                self.perform_nslookup()
+            elif option == "c":
+                self.perform_dns_lookup()
+            elif option == "d":
+                self.perform_reverse_dns_lookup()
+            elif option == "e":
+                self.perform_ssl_cert_lookup()
+            elif option == "f":
+                self.perform_http_headers_lookup()
+            elif option == "g":
+                self.perform_subdomain_lookup()
+            elif option == "h":
+                self._show_menu()
 
-    def perform_whois_lookup(self, domain):
+    def perform_whois_lookup(self):
+        for domain in self.data:
+            try:
+                w = whois.whois(domain)
+                result = f"Domain: {domain}\nRegistrar: {w.registrar}\nName Servers: {', '.join(w.name_servers)}"
+            except Exception as e:
+                result = f"Whois lookup failed for domain {domain}: {str(e)}"
+            self.results.append(result)
+
+    def perform_nslookup(self):
+        for domain in self.data:
+            try:
+                answers = dns.resolver.resolve(domain, "NS")
+                name_servers = [str(rdata) for rdata in answers]
+                result = f"Domain: {domain}\nName Servers: {', '.join(name_servers)}"
+            except Exception as e:
+                result = f"NSLookup failed for domain {domain}: {str(e)}"
+            self.results.append(result)
+
+    def perform_dns_lookup(self):
+        for domain in self.data:
+            try:
+                answers = dns.resolver.resolve(domain)
+                records = [str(rdata) for rdata in answers]
+                result = f"Domain: {domain}\nDNS Records: {', '.join(records)}"
+            except Exception as e:
+                result = f"DNS lookup failed for domain {domain}: {str(e)}"
+            self.results.append(result)
+
+    def perform_reverse_dns_lookup(self):
+        for ip_address in self.data:
+            try:
+                answers = dns.resolver.resolve_address(ip_address)
+                hostnames = [str(rdata) for rdata in answers]
+                result = f"IP Address: {ip_address}\nHostnames: {', '.join(hostnames)}"
+            except Exception as e:
+                result = (
+                    f"Reverse DNS lookup failed for IP address {ip_address}: {str(e)}"
+                )
+            self.results.append(result)
+
+    def perform_ssl_cert_lookup(self):
+        for domain in self.data:
+            try:
+                cert = ssl.get_server_certificate((domain, 443))
+                x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert)
+                expiration_date = x509.get_notAfter().decode()
+                result = f"Domain: {domain}\nExpiration Date: {expiration_date}"
+
+                cmd = f"sslscan {domain}"
+                sslscan_output = subprocess.check_output(cmd, shell=True, universal_newlines=True)
+                result += f"\n\nSSL Scan:\n{sslscan_output}"
+            except Exception as e:
+                result = f"SSL certificate lookup failed for domain {domain}: {str(e)}"
+            self.results.append(result)
+
+    def perform_http_headers_lookup(self):
+        for url in self.data:
+            try:
+                if not url.startswith("http://") and not url.startswith("https://"):
+                    url = "http://" + url
+
+                response = requests.head(url)
+                headers = response.headers
+                result = f"URL: {url}\nHeaders: {headers}"
+            except Exception as e:
+                result = f"HTTP headers lookup failed for URL {url}: {str(e)}"
+            self.results.append(result)
+
+    def perform_subdomain_lookup(self):
+        for domain in self.data:
+            try:
+                output = subprocess.check_output(["dig", f"{domain}", "+short", "+sub"])
+                subdomains = output.decode().strip().splitlines()
+                result = f"Domain: {domain}\nSubdomains: {', '.join(subdomains)}"
+            except Exception as e:
+                result = f"Subdomain lookup failed for domain {domain}: {str(e)}"
+            self.results.append(result)
+
+    def save_output_as_excel(self):
         try:
-            w = whois.whois(domain)
-            # Process the whois data and return the result
-            result = f"Domain: {domain}\nRegistrar: {w.registrar}\nName Servers: {', '.join(w.name_servers)}"
-        except Exception as e:
-            result = f"Whois lookup failed for domain {domain}: {str(e)}"
-        return result
-
-    def perform_nslookup(self, domain):
-        try:
-            answers = dns.resolver.resolve(domain, "NS")
-            name_servers = [str(rdata) for rdata in answers]
-            result = f"Domain: {domain}\nName Servers: {', '.join(name_servers)}"
-        except Exception as e:
-            result = f"NSLookup failed for domain {domain}: {str(e)}"
-        return result
-
-    def perform_dns_lookup(self, domain):
-        try:
-            answers = dns.resolver.resolve(domain)
-            records = [str(rdata) for rdata in answers]
-            result = f"Domain: {domain}\nDNS Records: {', '.join(records)}"
-        except Exception as e:
-            result = f"DNS lookup failed for domain {domain}: {str(e)}"
-        return result
-
-    def perform_reverse_dns_lookup(self, ip):
-        try:
-            answers = dns.resolver.resolve(dns.reversename.from_address(ip), "PTR")
-            hostnames = [str(rdata) for rdata in answers]
-            result = f"IP Address: {ip}\nHostnames: {', '.join(hostnames)}"
-        except Exception as e:
-            result = f"Reverse DNS lookup failed for IP {ip}: {str(e)}"
-        return result
-
-    def perform_ssl_cert_lookup(self, url):
-        try:
-            cert = ssl.get_server_certificate((url, 443))
-            x509 = OpenSSL.crypto.load_certificate(
-                OpenSSL.crypto.FILETYPE_PEM, cert.encode("latin-1")
-            )
-            subject = x509.get_subject()
-            ealtnames = []
-            for i in range(x509.get_extension_count()):
-                ext = x509.get_extension(i)
-                if ext.get_short_name() == b'subjectAltName':
-                    data = ext.get_data().decode(errors='ignore')
-                    altnames = [name.strip() for name in data.split(',')]
-
-            result = f"URL: {url}\n"
-            result += f"Subject: {subject.commonName}\n"
-            result += f"Altnames: {', '.join(altnames)}"
-
-            # SSL Scan
-            cmd = f"sslscan {url}"
-            sslscan_output = subprocess.check_output(cmd, shell=True, universal_newlines=True)
-            result += f"\n\nSSL Scan:\n{sslscan_output}"
-        except Exception as e:
-            result = f"SSL certificate lookup failed for URL {url}: {str(e)}"
-        return result
-
-
-    def perform_http_headers_lookup(self, url):
-        if not url.startswith("http://") and not url.startswith("https://"):
-            url = "http://" + url
-
-        try:
-            response = requests.head(url)
-            headers = response.headers
-            result = f"URL: {url}\nHTTP Headers:\n{headers}"
-        except Exception as e:
-            result = f"HTTP headers lookup failed for URL {url}: {str(e)}"
-        return result
-
-    def perform_subdomain_lookup(self, domain):
-        # Add code to perform subdomain lookup
-        pass
-
-    def save_output_as_excel(self, file_path):
-        try:
-            # Code to save output as Excel file
             wb = openpyxl.Workbook()
             sheet = wb.active
-            for i, result in enumerate(self.results):
-                sheet.cell(row=i + 1, column=1, value=result)
+            for i, result in enumerate(self.results, start=1):
+                sheet.cell(row=i, column=1, value=result)
+            file_path = self._get_save_file_path("xlsx")
             wb.save(file_path)
             wb.close()
+            print(f"Output saved as Excel file: {file_path}")
         except Exception as e:
             raise ValueError("Failed to save output as Excel file. Error: " + str(e))
 
-    def save_output_as_text(self, file_path):
+    def save_output_as_text(self):
         try:
-            # Code to save output as text file
+            file_path = self._get_save_file_path("txt")
             with open(file_path, "w") as file:
-                file.write("\n".join(self.results))
+                for result in self.results:
+                    file.write(result + "\n\n")
+            print(f"Output saved as Text file: {file_path}")
         except Exception as e:
             raise ValueError("Failed to save output as Text file. Error: " + str(e))
 
-    def save_output_as_xml(self, file_path):
+    def save_output_as_xml(self):
         try:
-            # Code to save output as XML file
             root = ET.Element("results")
             for result in self.results:
                 elem = ET.SubElement(root, "result")
                 elem.text = result
             tree = ET.ElementTree(root)
+            file_path = self._get_save_file_path("xml")
             tree.write(file_path)
+            print(f"Output saved as XML file: {file_path}")
         except Exception as e:
             raise ValueError("Failed to save output as XML file. Error: " + str(e))
 
-    def save_output_to_terminal(self):
-        # Code to display output in the terminal
-        for result in self.results:
-            print(result)
-
+    def _get_save_file_path(self, file_type):
+        root = Tk()
+        root.withdraw()
+        file_path = filedialog.asksaveasfilename(
+            defaultextension=f".{file_type.lower()}",
+            filetypes=[(f"{file_type} files", f"*.{file_type.lower()}")],
+        )
+        return file_path
+    
+    def print_output_to_terminal(self):
+        print("Output:")
+        for item in self.results:
+            print(item)
 
 def main():
     dp = DataProcessor()
     should_exit = False
 
-    while True:
-        print("\n1. Select how to input data:")
-        print("   a. Excel")
-        print("   b. XML")
-        print("   c. Text")
-        print("   d. Terminal")
-        print("   e. Exit")
-        input_option = input("Choose an option: ")
+    while not should_exit:
+        print("\nSelect data source:")
+        print("1. Excel file")
+        print("2. XML file")
+        print("3. Text file")
+        print("4. Terminal input")
+        print("5. Exit")
+        source_option = input("Enter option number: ")
 
-        if input_option == "a":
+        if source_option == "1":
             Tk().withdraw()  # Hide the root window
             file_path = askopenfilename(
-                title="Select Excel file"
-            )  # Show file selection dialog
-            dp.load_data_from_excel(file_path)
-        elif input_option == "b":
+                title="Select Excel file",
+                filetypes=[("Excel files", "*.xlsx"), ("All Files", "*.*")],
+            )
+            if file_path:
+                if dp.load_data_from_excel(file_path):
+                    dp.file_type = "Excel"
+            else:
+                print("No file selected.")
+                continue
+        elif source_option == "2":
             Tk().withdraw()
             file_path = askopenfilename(title="Select XML file")
             dp.load_data_from_xml(file_path)
-        elif input_option == "c":
+        elif source_option == "3":
             Tk().withdraw()
             file_path = askopenfilename(title="Select text file")
             dp.load_data_from_text(file_path)
-        elif input_option == "d":
+        elif source_option == "4":
             dp.load_data_from_terminal()
-        elif input_option == "e":
-            break  # Exit the loop and end the program
+        elif source_option == "5":
+            should_exit = True
         else:
             print("Invalid option. Please choose a valid option.")
             continue
 
-        while True:
-            print("\n2. Select functions to perform:")
-            print("   a. Whois lookup")
-            print("   b. NSLookup")
-            print("   c. DNS lookup")
-            print("   d. Reverse DNS lookup")
-            print("   e. SSL cert scan")
-            print("   f. HTTP headers")
-            print("   g. Subdomain lookup")
-            print("   h. Back to Menu")
-            function_options = input("Choose functions (comma-separated): ").split(",")
+        if not should_exit:
+            print("\nSelect functions to perform:")
+            print("a. WHOIS lookup")
+            print("b. NSLookup")
+            print("c. DNS lookup")
+            print("d. Reverse DNS lookup")
+            print("e. SSL certificate lookup")
+            print("f. HTTP headers lookup")
+            print("g. Subdomain lookup")
+            print("h. Show menu")
+            print("i. Save output as Excel file")
+            print("j. Save output as Text file")
+            print("k. Save output as XML file")
+            print("l. Print output to terminal")
+            print("m. Exit")
+            function_options = input("Enter options (e.g., 'ab' for WHOIS lookup and NSLookup): ")
 
             if "h" in function_options:
-                break  # Go back to the input option selection
+                dp._show_menu()
             else:
                 dp.perform_functions(function_options)
 
-            if "h" in function_options:
-                continue  # Go back to the input option selection
-
-            print("\n3. Select how to output the results:")
-            print("   a. Excel")
-            print("   b. Text")
-            print("   c. XML")
-            print("   d. Terminal")
-            print("   e. Back to Menu")
-            output_option = input("Choose an option: ")
-
-            if output_option == "a":
-                Tk().withdraw()
-                file_path = asksaveasfilename(title="Save Excel file")
-                dp.save_output_as_excel(file_path)
-            elif output_option == "b":
-                Tk().withdraw()
-                file_path = asksaveasfilename(title="Save Text file")
-                dp.save_output_as_text(file_path)
-            elif output_option == "c":
-                Tk().withdraw()
-                file_path = asksaveasfilename(title="Save XML file")
-                dp.save_output_as_xml(file_path)
-            elif output_option == "d":
-                dp.save_output_to_terminal()
-            elif output_option == "e":
-                continue  # Go back to the input option selection
-            else:
-                print("Invalid option. Please choose a valid option.")
-
+            if "i" in function_options:
+                dp.save_output_as_excel()
+            if "j" in function_options:
+                dp.save_output_as_text()
+            if "k" in function_options:
+                dp.save_output_as_xml()
+            if "l" in function_options:
+                dp.print_output_to_terminal()
 
 if __name__ == "__main__":
     main()
